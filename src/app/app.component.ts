1991ff380ad37e093d92e3dc2bea2d51f1b04f35
donaldteghen <donaldteghen@gmail.com>
2021-02-06 22:15:44 +0300

    Profile app setup and pdf converter functio setup and tested.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {jsPDF} from 'jspdf';
+import html2canvas from 'html2canvas'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+ 
+ constructor(){}
+ 
+ convetToPDF(){
+  var data = document.getElementById('contentToConvert');
+  html2canvas(data).then(canvas =>{
+    var imgWidth = 208;
+    var pageHeight = 295;
+    var imgHeight = canvas.height * imgWidth / canvas.width;
+    var heightLeft = imgHeight;
+
+    const contentDataUrl = canvas.toDataURL('image/jpeg')
+    let pdf = new jsPDF();
+    pdf.addImage(contentDataUrl, 'JPEG', 0, 0, imgWidth,imgHeight, 'FAST')
+    pdf.save('pdftester')
+  })
+  }
 }
