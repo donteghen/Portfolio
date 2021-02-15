be72e06d72572371fea0639f96054da9a3c5e395
donaldteghen <donaldteghen@gmail.com>
2021-02-15 20:33:40 +0300

    Add a mobile view for side tab menu. Added an html mailer script for the contact form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,14 @@ import {ScrollPanelModule} from 'primeng/scrollpanel';
 import {ButtonModule} from 'primeng/button'
 import { CommonModule } from '@angular/common';
 import { ProjectService } from './services/project.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
    AppComponent,
   ],
   imports: [
-    BrowserModule, BrowserAnimationsModule, ScrollPanelModule, ButtonModule, CommonModule, 
+    BrowserModule, BrowserAnimationsModule, ScrollPanelModule, ButtonModule, CommonModule, HttpClientModule,
     AppRoutingModule
   ],
   providers: [ProjectService],
--- src/app/ui/contact/contact.component.ts
+++ src/app/ui/contact/contact.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
@@ -8,7 +9,10 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class ContactComponent implements OnInit {
 form : FormGroup;
-  constructor() { }
+isLoading :boolean =false;
+submitted : boolean =false;
+responseMessage: string;
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
@@ -27,9 +31,36 @@ form : FormGroup;
   get message(){
     return this.form.get('message');
   }
-  sendEmail(form){
-    console.log(form);
+  sendEmail(){
+    if(this.form.valid){
+      var formData = new FormData();
+      formData.append("name", this.form.get("name").value);
+      formData.append("email", this.form.get("email").value);
+      formData.append("message", this.form.get("message").value);
+      this.isLoading = true; // sending the post request async so it's in progress
+      this.submitted = false; // hide the response message on multiple submits
+      this.http.post("https://script.google.com/macros/s/AKfycbxYz2ez3OnG_9aPj6h9c0Td9hTFOjr54jWbdYTWQZ_WWXi39lq4KkPReQ/exec", formData).subscribe(
+        (response) => {
+          // choose the response message
+          if (response["result"] == "success") {
+            this.responseMessage = "Thanks for getting in touch! I'll get back to you soonest!";
+          } else {
+            this.responseMessage = "Oops! Something went wrong... Please reload the page and try again.";
+          }
+          this.form.enable(); // re enable the form after a success
+          this.submitted = true; // show the response message
+          this.isLoading = false; // re enable the submit button
+        },
+        (error) => {
+          this.responseMessage = "Oops! An error occurred... Reload the page and try again.";
+          this.form.enable(); // re enable the form after a success
+          this.submitted = true; // show the response message
+          this.isLoading = false; // re enable the submit button
+          console.log(error);
+        }
+      );
+    }
   }
-
+ 
 
 }
