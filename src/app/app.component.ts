3a8e99f758d7a99c7ce1e7111581b8406859f33c
donaldteghen <donaldteghen@gmail.com>
2021-02-16 13:42:45 +0300

    Added a downloadable pdf to the download button in the resume template. Remove the jspdf dependency from the porject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import {jsPDF} from 'jspdf';
-import html2canvas from 'html2canvas'
+
 
 @Component({
   selector: 'app-root',
@@ -12,19 +11,5 @@ export class AppComponent {
  
  constructor(){}
 
- // pdf maker function
- convetToPDF(){
-  var data = document.getElementById('contentToConvert');
-  html2canvas(data).then(canvas =>{
-    var imgWidth = 208;
-    var pageHeight = 295;
-    var imgHeight = canvas.height * imgWidth / canvas.width;
-    var heightLeft = imgHeight;
-
-    const contentDataUrl = canvas.toDataURL('image/jpeg')
-    let pdf = new jsPDF();
-    pdf.addImage(contentDataUrl, 'JPEG', 0, 0, imgWidth,imgHeight, 'FAST')
-    pdf.save('pdftester')
-  })
-  }
+ 
 }
